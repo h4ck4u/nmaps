8fe09546497ae04f6c5052377b0baf2034747104
b3t4m3 <80868159+bet4m3@users.noreply.github.com>
2022-08-15 12:44:22 -0600

    tito s4vi

--- nmaps.sh
+++ nmaps.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-ports=$(nmap -p- -sS --min-rate 5000 --open -n $1 | grep  '^[0-9]' | cut -d '/' -f 1 | xargs | tr ' ' ',')
-nmap -sCV -p$ports $1 -oN Targeted
+function ctrl_c(){
+ echo -e "\n\n [!]Exiting...\n"
+ tput cnorm; exit 1
+}
+
+trap ctrl_c INT
+
+declare -r ip_address="$1"
+
+tput civis
+if ["$(echo $ip_address | grep -oP "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")"]; then
+  ports=$(nmap -p- -sS --min-rate 5000 --open -n $1 | grep  '^[0-9]' | cut -d '/' -f 1 | xargs | tr ' ' ',')
+  nmap -sCV -p$ports $1 -oN Targeted
+fi
+
+tput cnorm
